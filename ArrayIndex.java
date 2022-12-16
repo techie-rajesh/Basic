7c48850acc59a897d37e475a1d21330d931369b4
niles131013 <nileshnaidu1393@gmail.com>
2022-12-16 12:58:54 +0530

    new commit

--- ArrayIndex.java
+++ ArrayIndex.java
@@ -3,8 +3,9 @@ package Exception;
 public class ArrayIndex {
 	public static void main(String[] args) {
 		
-		int[]a= {1,2,4,5};
+		int[]a= {1,2,4,10,15};
 		System.out.println(a[4]);
+		System.out.println(a[5]);
 		
 	}
 
