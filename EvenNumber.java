55f7b81b427fb075a70984ebf0c82a360c5a83e5
techie-rajesh <vrajeshraju1@gmail.com>
2022-12-23 10:25:23 +0530

    Add basic prog

---
+++ EvenNumber.java
@@ -0,0 +1,17 @@
+package forLoop;
+
+public class EvenNumber {
+
+	public static void main(String[] args) 
+	{
+		/*for(int i=1;i<=10;i++)
+		{
+			if(i%2==0)
+				System.out.println(i);
+		}*/
+		for(int i=2;i<=10;i+=2)
+			System.out.println(i);
+
+	}
+
+}
